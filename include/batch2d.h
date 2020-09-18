720d75a7477008445df34dcda9b78d1e4b8c1bf4
MagicalMan <dowdtomas@gmail.com>
2020-09-18 22:22:26 +1000

    Added simple shaders and slightly tweaked the system

--- include/batch2d.h
+++ include/batch2d.h
@@ -6,5 +6,5 @@ void Batch_system_init(unsigned short max_batches); // sets up batches
 
 BatchID Batch_add(unsigned int max_pool_size);
 void Batch_drawRect(BatchID batch, int x, int y, int width, int height); // add some way to pass textures
-void Batch_display(BatchID batch);
+void Batch_flush(BatchID batch); // draws instances =)
 void Batch_remove(BatchID batch);
--- src/batch2d.c
+++ src/batch2d.c
@@ -27,6 +27,25 @@ typedef struct {
 //----------------------------------------------------------------------------------------------------
 
 
+static const char* vertex_shader_source =
+"#version 330\n"
+"\n"
+"layout (location = 0) in vec2 aPos;\n"
+"\n"
+"void main() {\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"}\n";
+static const char* frag_shader_source = 
+"#version 330\n"
+"\n"
+"out vec4 FragColor"
+"\n"
+"void main() {\n"
+"	FragColor = vec4(1.0, 1.0, 0, 1.0);\n"
+"}\n";
+
+static unsigned int shader_program;
+
 static unsigned short max_batch_count = 0;
 static unsigned short active_batch_count = 0;
 static Batch* batches;
@@ -36,8 +55,49 @@ static Batch* batches;
 
 
 void Batch_system_init(unsigned short max_batches) {
+	// allocate batch memory
 	max_batch_count = max_batches;
 	batches = (Batch*) malloc(sizeof(Batch) * max_batch_count);
+
+	// compile shader programs
+	
+	int success;
+	char info_log[512];
+
+	unsigned int vertex_shader;
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if(!success) {
+		glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+		printf("ERROR SHADER DIDN'T COMPILE %i: %s\n", __LINE__, info_log);
+	}
+
+	unsigned int frag_shader;
+	
+	frag_shader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(frag_shader, 1, &frag_shader_source, NULL);
+	glCompileShader(frag_shader);
+
+	glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+	if(!success) {
+		glGetShaderInfoLog(frag_shader, 512, NULL, info_log);
+		printf("ERROR SHADER DIDN'T COMPILE %i: %s\n", __LINE__, info_log);
+	}
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, frag_shader);
+	glLinkProgram(shader_program);
+
+	// NOTE: Currently no error checking on the link status
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(frag_shader);
 }
 
 
@@ -67,9 +127,8 @@ BatchID Batch_create(unsigned int max_pool_size) {
 	return active_batch_count;
 }
 
-void Batch_destroy(Batch* batch) {
-	
-	free(batch);
+// REWORK
+void Batch_remove(BatchID id) {
 }
 
 
@@ -85,23 +144,38 @@ void Batch_drawRect(BatchID id, int x, int y, int width, int height) {
 	verts[1].x = x + width;
 	verts[1].y = y + height;
 
-	Batch* batch = &batches[active_batch_count];
+
+
+	Batch* batch = &batches[id];
+
+	// if buffers are full, flush before proceeding
+	if(batch->current_index >= batch->max_pool_size) {
+		Batch_flush(id);
+	}
 
 	glBindVertexArray(batch->vao);
 
-	// Allocate 2 verticies per draw
+	// allocate 2 vertices per rect
 	glBindBuffer(GL_ARRAY_BUFFER, batch->vbo);
 	glBufferSubData(GL_ARRAY_BUFFER, batch->current_index * 2 * sizeof(Vertex), sizeof(Vertex) * 2, verts);
+
+	// allocate indeces
+
+	batch->current_index++;
 }
 
 
 //----------------------------------------------------------------------------------------------------
 
 
-void Batch_display(BatchID id) {
+void Batch_flush(BatchID id) {
+	Batch* batch = &batches[id];
+
+	// draw instanced elements
+
+	// reset batch parameters
+	batch->current_index = 0;
 }
 
 
 //----------------------------------------------------------------------------------------------------
-
-
